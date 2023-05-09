80d6adf83918fe4a9a0307e4d57881c24b7355e3
siena95 <131760300+siena95@users.noreply.github.com>
2023-05-10 02:40:58 +0900

    패키지로 올려야하는데..
    자바파일만 일단 올려둠...

---
+++ src/main/java/env/OtherdogsList.java
@@ -0,0 +1,36 @@
+package diary;
+
+public class OtherdogsList {
+
+	String[] imgList = {"../resource/dog0.jpg", "../resource/dog1.jpg", "../resource/dog2.jpg","../resource/dog3.jpg", "../resource/dog4.jpg","../resource/dog5.jpg",
+						"../resource/dog6.jpg", "../resource/dog7.jpg", "../resource/dog8.jpg", "../resource/dog9.jpg", "../resource/dog10.jpg", "../resource/dog11.jpg"}; 
+	
+	String[] titleList = {"생일 파티를 했어요!", "방해꾼", "찰칵!", "낮잠이 좋아", "즐거운 산책시간", "쓰담쓰담", "어질리티 훈련",  
+						  "챱챱 물 마시기", "가족의 탄생",  "얌전히 기다리기!", "비온 뒤 맑음!", "꼼짝마!"};
+	
+	String[] contentList = {"오늘 생일을 맞은 코코!<br>동생 껌딱지 모모와 함께 같이 사진 찍었어요! 맛있는 간식도 먹고 즐거웠답니다~",
+							"선생님들의 휴식시간에도 놀아달라고 찾아온 토비! 귀여워서 한참을 예뻐해주었어요~",
+							"오늘은 증명사진 찍는 날! 모델이 된 듯 얌전한 츄츄에 선생님들은 물론 사진기사님도 감탄했답니다~",
+							"깨워도 못들은 척~장군이는 피곤했는지 하루종일 쿨쿨 잠만 잤어요!",
+							"친구 몽실이와 달리기 대결 중인 시루! 사이좋게 나란히 뛰어오네요~",
+							"무한 쓰담쓰담을 받으려고 선생님 발밑에만 머무는 쪼꼬! 중간에 멈추면 계속하라고 보채요~",
+							"훈련 3일차 레오! 이젠 장애물에 익숙해졌는지 곧잘 오르고 내리네요~",
+							"새로온 친구를 격하게 환영해준 두부! 갈증이 났는지 한참동안 물을 마셨답니다~",
+							"지난달에 출산한 마루의 새끼들이 놀러왔어요~친구들도 마루의 새끼들과 잘 어울렸어요!",
+							"기다려 훈련 중인 루루! 3초가 3분같이 느껴지는지 끙끙거리면서도 잘 기다리는 착한 아이예요~",
+							"어제 비가 내려서 내내 안에만 있었던 비니와 미니! 햇살을 보고 활짝 웃는 게 너무 예쁘네요~",
+							"풀밭에서 놀던 하쿠가 갑자기 움직이던 곳엔! 작은 벌레를 따라 폴짝폴짝 뛰어다니는 게 재밌네요~"};
+
+	public String[] getImgList() {
+		return imgList;
+	}
+
+	public String[] getTitleList() {
+		return titleList;
+	}
+
+	public String[] getContentList() {
+		return contentList;
+	}
+
+}
